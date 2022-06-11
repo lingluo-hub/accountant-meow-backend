64d74f2bd8f29b51cadd2d338f7938aee7ca114c
Ling Luo <25563773+lingluo-hub@users.noreply.github.com>
2022-06-11 21:14:37 +0800

    controller 层初步完成单元测试类，删去 Type

--- src/main/resources/db/migration/V1.1__Init_Data.sql
+++ src/main/resources/db/migration/V1.1__Init_Data.sql
@@ -1,3 +1,24 @@
 INSERT INTO budget (userid, budget) VALUES ('627875eda49a5d62769d415a', 1500);
 INSERT INTO budget (userid, budget) VALUES ('6267e484f9b3dc3313893ce0', 1500);
 INSERT INTO budget (userid, budget) VALUES ('626e437b9dc68d14cdb4e2f6', 3000);
+
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('医疗', 2131165429, '', 50, '2022年05月16日 00:00', 2022, 5, 16, 0, '2022-05-28 13:16:34.179342', '626e437b9dc68d14cdb4e2f6');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('烟酒茶', 2131165427, '', 50, '2022年05月28日 21:16', 2022, 5, 28, 0, '2022-05-28 13:17:13.337376', '626e437b9dc68d14cdb4e2f6');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('零食', 2131165406, '薯片', 8, '2022年05月30日 15:16', 2022, 5, 30, 0, '2022-05-30 07:17:02.134562', '626e437b9dc68d14cdb4e2f6');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('交通', 2131165378, 'null', 25, '2022年05月10日 16:43', 2022, 5, 10, 0, '2022-05-10 16:43:57.861879', '627875eda49a5d62769d415a');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('住宅', 2131165410, 'null', 248, '2022年05月10日 16:44', 2022, 5, 10, 0, '2022-05-10 16:44:12.638695', '627875eda49a5d62769d415a');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('其他', 2131165389, 'null', 554, '2022年05月10日 16:45', 2022, 5, 10, 0, '2022-05-10 16:45:34.733524', '627875eda49a5d62769d415a');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('水电煤', 2131165397, 'null', 246, '2022年05月08日 00:00', 2022, 5, 8, 0, '2022-05-10 16:45:54.157215', '627875eda49a5d62769d415a');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('薪资', 2131165366, 'null', 25, '2022年05月10日 16:49', 2022, 5, 10, 1, '2022-05-10 16:49:11.584857', '627875eda49a5d62769d415a');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('水电煤', 2131165397, '', 50, '2022年05月31日 16:47', 2022, 5, 31, 0, '2022-05-31 08:47:53.955216', '626e437b9dc68d14cdb4e2f6');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('水电煤', 2131165397, 'null', 548, '2022年05月14日 14:38', 2022, 5, 14, 0, '2022-05-14 06:38:17.583615', '6267e484f9b3dc3313893ce0');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('购物', 2131165347, 'null', 566, '2021年12月24日 00:00', 2021, 12, 24, 0, '2022-05-14 06:38:33.607331', '6267e484f9b3dc3313893ce0');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('人情往来', 2131165414, '', 50, '2022年06月07日 14:12', 2022, 6, 7, 0, '2022-06-07 06:12:17.678824', '629de9e4da98421dc9cb1b9b');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('二手交易', 2131165373, '', 5, '2022年06月07日 14:12', 2022, 6, 7, 1, '2022-06-07 06:12:30.959936', '629de9e4da98421dc9cb1b9b');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('零食', 2131165425, null, 99, '2022年06月07日 15:13', 2022, 6, 7, 0, '2022-06-07 07:14:28.461180', '629ef973cabd49083a3323c1');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('餐饮', 2131165341, 'null', 555, '2021年12月31日 00:00', 2021, 12, 31, 0, '2022-05-14 09:53:19.297948', '6267e484f9b3dc3313893ce0');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('交通', 2131558410, '名年法马小七则手品团所事给体验。', 492.2, '15:52:49', 2022, 5, 26, 0, '2022-05-26 21:15:29.719985', '6267e484f9b3dc3313893ce0');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('餐饮', 2131165364, '', 10, '2022年06月10日 15:08', 2022, 6, 10, 0, '2022-06-10 07:08:09.558313', '629efd76b4ce73214b197501');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('娱乐', 2131165431, '', 50, '2022年06月10日 15:08', 2022, 6, 10, 0, '2022-06-10 07:08:12.367082', '629efd76b4ce73214b197501');
+INSERT INTO public.accounttb (typename, simageid, beizhu, money, time, year, month, day, kind, timest, userid) VALUES ('二手交易', 2131165373, '', 50, '2022年06月10日 15:08', 2022, 6, 10, 1, '2022-06-10 07:08:15.636689', '629efd76b4ce73214b197501');
+
