d6a8019336f476a88ae4a92674554efa89c4a955
Ling Luo <25563773+lingluo-hub@users.noreply.github.com>
2022-06-12 13:45:07 +0800

    优化实体结构

--- src/main/java/com/meow/accountant/entity/BarCharItem.java
+++ src/main/java/com/meow/accountant/entity/BarCharItem.java
@@ -2,11 +2,8 @@ package com.meow.accountant.entity;
 
 import lombok.*;
 
-@Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class BarCharItem {
     private int year;
     private int month;
